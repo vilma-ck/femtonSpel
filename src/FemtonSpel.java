539937520b2c9b5aa1755c9c82f2f30cdb2d5ed5
Vilma Kaijser <vilma.kaijser@gmail.com>
2020-10-22 11:15:27 +0200

    variables for buttons

--- src/FemtonSpel.java
+++ src/FemtonSpel.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.jar.JarFile;
 
 /**
  * Created by Vilma Couturier Kaijser
@@ -17,10 +16,32 @@ public class FemtonSpel extends JFrame {
     private List<JButton> buttonsOrdered = new ArrayList<>();
     private List<JButton> buttonsGame = new ArrayList<>();
 
+    private JButton b1 = new JButton("1");
+    private JButton b2 = new JButton("2");
+    private JButton b3 = new JButton("3");
+    private JButton b4 = new JButton("4");
+    private JButton b5 = new JButton("5");
+    private JButton b6 = new JButton("6");
+    private JButton b7 = new JButton("7");
+    private JButton b8 = new JButton("8");
+    private JButton b9 = new JButton("9");
+    private JButton b10 = new JButton("10");
+    private JButton b11 = new JButton("11");
+    private JButton b12 = new JButton("12");
+    private JButton b13 = new JButton("13");
+    private JButton b14 = new JButton("14");
+    private JButton b15 = new JButton("15");
+    private JButton bNull = new JButton();
+
     // testcomment
 
     public FemtonSpel(){
-        makeButtons();
+        buttonsGame.add(b1); buttonsGame.add(b2); buttonsGame.add(b3); buttonsGame.add(b4); buttonsGame.add(b5);
+        buttonsGame.add(b6); buttonsGame.add(b7); buttonsGame.add(b8); buttonsGame.add(b9); buttonsGame.add(b10);
+        buttonsGame.add(b11); buttonsGame.add(b12); buttonsGame.add(b13); buttonsGame.add(b14); buttonsGame.add(b15);
+        buttonsGame.add(bNull);
+
+        //makeButtons();
 
         add(panel);
 
@@ -45,7 +66,6 @@ public class FemtonSpel extends JFrame {
             buttonsOrdered.add(new JButton(String.valueOf(i)));
             buttonsGame.add(new JButton(String.valueOf(i)));
             //System.out.println(i);
-
         }
     }
 
